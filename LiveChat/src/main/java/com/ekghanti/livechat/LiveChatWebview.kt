27e087bbd7418a30a9c5dc4f977c5eb0928c570f
janakbastakoti <janakbastakoti@gmail.com>
2024-09-08 17:42:09 +0545

    webview

--- LiveChat/src/main/java/com/ekghanti/livechat/LiveChatWebview.kt
+++ LiveChat/src/main/java/com/ekghanti/livechat/LiveChatWebview.kt
@@ -1,6 +1,35 @@
 package com.ekghanti.livechat
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.webkit.WebSettings
+import android.webkit.WebView
 import androidx.fragment.app.Fragment
 
 class LiveChatWebview: Fragment(R.layout.livechat_webview) {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.livechat_webview, container, false)
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Initialize the WebView and set its settings
+        val webView: WebView = view.findViewById(R.id.webView)
+        val webSettings: WebSettings = webView.settings
+        webSettings.javaScriptEnabled = true
+        webSettings.domStorageEnabled = true
+        webSettings.mediaPlaybackRequiresUserGesture = false
+        webSettings.allowFileAccess = true
+        webSettings.setSupportZoom(true)
+
+        // Load the URL
+        webView.loadUrl("https://chat.orbit360.cx:8443/chat/fd0caaa3-f1cb-4d0a-a452-171f21ec16ee")
+    }
+
 }
